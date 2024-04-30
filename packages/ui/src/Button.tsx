b79b24c84a2e2d2ffc83562ee692be8bf89fd8e5
Isaiah Smith <jordankid93@gmail.com>
2024-04-29 18:29:20 -0700

    chore: fix ui linting

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import clsx from "clsx"
-import { ReactNode } from "react"
+import { clsx } from "clsx"
+import { type ReactNode } from "react"
 
 interface ButtonProps {
   children: ReactNode
@@ -13,7 +13,10 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={clsx("ui-text-blue-500", className)}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => {
+        console.info(`Hello from your ${appName} app!`)
+      }}
+      type="button"
     >
       {children}
     </button>
